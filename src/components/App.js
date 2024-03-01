197e99c02d3de4735ea67acf65a3461bfa49f78c
ArjayBuen <arjaybuen10@gmail.com>
2024-03-01 23:10:28 +0800

    dividing components into smaller

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card from './Card';
 import contacts from '../contact';
+import Avatar from './Avatar';
 
 function App() {
   // console.log(contacts);
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://th.bing.com/th/id/OIP.tfUH2--rIS1BIHdHC6nktAHaJ4?w=186&h=248&c=7&r=0&o=5&pid=1.7"/>
       <Card name={contacts[0].name} 
         tel={contacts[0].phone} 
         email={contacts[0].email}
